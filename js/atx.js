e1a237fb2c23ac8f30d74f3b93a8dfd986ce8448
Letha Covey <letha@shane-williamss-macbook-pro.local>
2016-01-24 10:59:46 -0600

    initial ATX version

--- js/atx.js
+++ js/atx.js
@@ -2,21 +2,15 @@
   'use strict';
 
   var imgs = [
-    ['images/dt-ft-worth.jpg', "Ken Lund", "https://www.flickr.com/photos/kenlund/68218446"],
-    ['images/dt-dallas.jpg', "Raging Wire", "https://www.flickr.com/photos/ragingwire/3334765721"],
-    ['images/bp-arlington.jpg', "Bo Nash", "https://www.flickr.com/photos/sidehike/309394754"],
-    ['images/cowboys-stadium.jpg', "joevare", "https://www.flickr.com/photos/joevare/5629027097"],
-    ['images/dallas-arboretum.jpg', "Rodney", "https://www.flickr.com/photos/rjhuttondfw/9937691186"],
-    ['images/dealey-plaza.jpg', "CameliaTWU", "https://www.flickr.com/photos/cameliatwu/10972543013"],
-    ['images/denton.jpg', "Brandi Korte", "https://www.flickr.com/photos/branditressler/8401359196"],
-    ['images/ftw-stockyard.jpg', "Nicolas Henderson", "https://www.flickr.com/photos/texasbackroads/8126754604"],
-    ['images/ftw-water-garden.jpg', "Nicolas Henderson", "https://www.flickr.com/photos/texasbackroads/7004491381"],
-    ['images/gaylord-texan.jpg', "Dell Inc.", "https://www.flickr.com/photos/dellphotos/5083385705"],
-    ['images/lonestar-park.jpg', "Travis Isaacs", "https://www.flickr.com/photos/tbisaacs/2406344139"],
-    ['images/shops-legacy.jpg', "Tony Moore", "https://www.flickr.com/photos/soulrider/2963640409"],
-    ['images/southfork.jpg', "Adam Simmons", "https://www.flickr.com/photos/mr-numb/13936057127"],
-    ['images/basshall.jpg', "Bron Praslicka", "https://www.flickr.com/photos/bronpraslicka/6439645075/"]
-  ],
+    ['images/ATX_capitol2.jpg', "Elana"],
+    ['images/ATX_franklin2.jpg',  "Elana"],
+    ['images/ATX_historystar.jpg', "Elana"],
+    ['images/ATX_leaves.jpg', "Elana"],
+    ['images/ATX_maze2.jpg', "Elana"],
+    ['images/ATX_tower2.jpg', "Elana"],
+    ['images/ATX_night.jpg', "Elana"],
+    ['images/ATX_skyline2.jpg', "Elana"]
+],
     Cache = [],
     currentIndex = -1,
     rotateDuration = 8 * 1000,
